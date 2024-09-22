5300275a62a2ed7e476ba0885ee4ecdf57e0a60f
Piyush620 <107906557+Piyush620@users.noreply.github.com>
2024-09-22 21:52:38 +0530

    server.py

--- server.py
+++ server.py
@@ -1,17 +1,15 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 import product_dao
 from sql_connection import get_sql_connection
 
-
 app = Flask(__name__)
 connection = get_sql_connection()
-@app.route('/getProducts',methods =['GET'])
+@app.route('/getProducts', methods=['GET'])
 def get_products():
     products = product_dao.get_all_products(connection)
     response = jsonify(products)
-    response.headers.add('Access-Control-Allow-origin','*')
-    return response 
-
-if __name__ =="__main__":
-    print("Starting Python Flask Server For Grocery Store Mangement System")
-    app.run(port=5000)
+    response.headers.add('Access-Control-Allow-Origin', '*')  
+    return response
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Grocery Store Management System")
+    app.run(port=5000, debug=True)
