59a88338ac6c51f934c4b434ebbcc7dfc22e8553
Piyush620 <107906557+Piyush620@users.noreply.github.com>
2024-09-22 22:09:23 +0530

    server.py

--- server.py
+++ server.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import product_dao
 from sql_connection import get_sql_connection
 
 app = Flask(__name__)
+CORS(app)  # Add CORS support
+
 connection = get_sql_connection()
+
 @app.route('/getProducts', methods=['GET'])
 def get_products():
     products = product_dao.get_all_products(connection)
-    response = jsonify(products)
-    response.headers.add('Access-Control-Allow-Origin', '*')  
-    return response
+    return jsonify(products)
+
 if __name__ == "__main__":
     print("Starting Python Flask Server For Grocery Store Management System")
     app.run(port=5000, debug=True)
